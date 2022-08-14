0fbaebd90ad04de95a7ac18696cb444df3a8736b
mahirlabibdihan <mahirlabibdihan@gmail.com>
2022-08-15 00:34:51 +0600

    Public panel changed

--- src/components/Panels/TutorPanel.jsx
+++ src/components/Panels/TutorPanel.jsx
@@ -9,17 +9,140 @@ import GlobalContext from "../../store/GlobalContext";
 import TutorsController from "../../controller/tutorsController";
 import Rating from "@mui/material/Rating";
 import "./TutorPanel.scss";
+import { MultiLineField } from "../InputFields";
+import Feedback from "../Cards/Feedback";
+import { useLocation } from "react-router-dom";
 const tutorsController = new TutorsController();
 const tutionController = new TutionController();
+
+const ProfilePanel = (props) => {
+  const location = useLocation();
+  console.log(props.feedbacks);
+  return (
+    <>
+      <TutorProfile tutor={props.tutor} education={props.education} />
+      <Divider />
+      {location.pathname.split("/")[1] === "home" ? (
+        <div className="feedback-container">
+          <h6 className="standard-font-1 text-center mb-0">Feedbacks</h6>
+          <Divider />
+          {props.feedbacks.map((feedback) => (
+            <Feedback rating={feedback.RATING} review={feedback.REVIEW} />
+          ))}
+        </div>
+      ) : (
+        <></>
+      )}
+      {props.tution.STATUS === null ? (
+        <>
+          <Button
+            variant="contained"
+            className="blue-button full-width"
+            onClick={(e) => props.setPage("offer")}
+          >
+            Offer
+          </Button>
+        </>
+      ) : (
+        <div>
+          {props.tution.STATUS === "PENDING" ? (
+            <div className="vbox">
+              <TutionDetails
+                tution={props.tution}
+                tutorName={props.tutor.NAME}
+              />
+              <Button
+                variant="Contained"
+                className="red-button full-width"
+                onClick={props.handleCancel}
+              >
+                Cancel
+              </Button>
+            </div>
+          ) : location.pathname.split("/")[1] === "home" ? (
+            <></>
+          ) : (
+            <div className="flex-center vbox">
+              <TutionDetails
+                tution={props.tution}
+                tutorName={props.tutor.NAME}
+              />
+              {/* <Divider /> */}
+              <div className="rating-container poppins-font">
+                {`How would you rate ${
+                  props.tutor.GENDER === "Male" ? "him" : "her"
+                }?`}
+              </div>
+              <Rating
+                className="rating"
+                name="simple-controlled"
+                value={props.feedback.rating}
+                size="large"
+                onChange={props.handleChange("rating")}
+              />
+              <div className="feedback-form vbox">
+                <MultiLineField
+                  rows={3}
+                  label={"Feedback"}
+                  type="text"
+                  value={props.feedback.review}
+                  id={"review"}
+                  onChange={props.handleChange}
+                />
+                <Button
+                  variant="Contained"
+                  className="blue-button full-width"
+                  onClick={() => {
+                    props.submitFeedback();
+                  }}
+                >
+                  Submit
+                </Button>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+      {/* <Button
+        variant="contained"
+        className="blue-button full-width"
+        onClick={(e) => props.setPage("offer")}
+      >
+        Feedbacks
+      </Button> */}
+    </>
+  );
+};
 const TutorPanel = (props) => {
-  // console.log(props);
+  console.log("TUTOR", props);
+  const location = useLocation();
   const globalCtx = useContext(GlobalContext);
-  const [rating, setRating] = useState(props.tution.RATING);
+  const [page, setPage] = useState("profile");
+  const [feedback, setFeedback] = useState({
+    rating: props.tution.RATING,
+    review: props.tution.REVIEW,
+  });
+  const [feedbacks, setFeedbacks] = useState([]);
+  const handleChange = (prop) => (event) => {
+    setFeedback({ ...feedback, [prop]: event.target.value });
+  };
   const [education, setEducation] = useState([]);
   const [needUpdate, setNeedUpdate] = useState(false);
   const setEducationList = async () => {
     const result = await tutorsController.getEducation(props.tutor.USER_ID);
     if (result.success) setEducation(result.data);
+  };
+  const submitFeedback = async () => {
+    // setRating(newValue);
+    console.log("CLICKED");
+    const result = await tutionController.rate(
+      props.tutor.USER_ID,
+      feedback.rating,
+      feedback.review
+    );
+    if (result.success) {
+      globalCtx.setPendingUpdate(true);
+    }
   };
   const handleCancel = async () => {
     const result = await tutionController.cancelOffer(props.tutor.USER_ID);
@@ -27,62 +150,53 @@ const TutorPanel = (props) => {
       globalCtx.setPendingUpdate(true);
     }
   };
-  useEffect(() => {
-    setEducationList();
-  }, [props]);
-  const updateRating = async (newValue) => {
-    setRating(newValue);
-    const result = await tutionController.rate(props.tutor.USER_ID, newValue);
-    if (result.success) {
-      globalCtx.setPendingUpdate(true);
+  const setFeedbackList = async () => {
+    const res = await tutionController.getFeedbacks(props.tutor.USER_ID);
+    console.log("Feedbacks", res);
+    if (res.success) {
+      setFeedbacks(res.data);
     }
   };
+  useEffect(() => {
+    setEducationList();
+    setPage("profile");
+    setFeedback({
+      rating: props.tution.RATING,
+      review: props.tution.REVIEW,
+    });
+    // console.log("=>", props);
+    if (location.pathname.split("/")[1] === "home") {
+      setFeedbackList();
+    }
+  }, [props]);
+
   return (
     <div className="tutor-panel">
       {props.tutor === undefined ? (
         <></>
-      ) : (
+      ) : page === "offer" ? (
         <>
-          <TutorProfile tutor={props.tutor} education={education} />
-          <Divider />
-          {props.tution.STATUS === null ? (
-            <OfferForm tutor_id={props.tutor.USER_ID} tution={props.tution} />
-          ) : (
-            <>
-              <TutionDetails
-                tution={props.tution}
-                tutorName={props.tutor.NAME}
-              />
-              {props.tution.STATUS === "PENDING" ? (
-                <Button
-                  variant="Contained"
-                  className="red-button full-width"
-                  onClick={handleCancel}
-                >
-                  Cancel
-                </Button>
-              ) : (
-                <div className="flex-center vbox">
-                  <Divider />
-                  <div className="rating-container poppins-font">
-                    {`How would you rate ${
-                      props.tutor.GENDER === "Male" ? "him" : "her"
-                    }?`}
-                  </div>
-                  <Rating
-                    className="rating"
-                    name="simple-controlled"
-                    value={rating}
-                    size="large"
-                    onChange={(event, newValue) => {
-                      updateRating(newValue);
-                    }}
-                  />
-                </div>
-              )}
-            </>
-          )}
+          <OfferForm tutor_id={props.tutor.USER_ID} tution={props.tution} />
+          <Button
+            variant="contained"
+            className="red-button full-width"
+            onClick={(e) => setPage("profile")}
+          >
+            Cancel
+          </Button>
         </>
+      ) : (
+        <ProfilePanel
+          tutor={props.tutor}
+          tution={props.tution}
+          feedback={feedback}
+          feedbacks={feedbacks}
+          submitFeedback={submitFeedback}
+          handleCancel={handleCancel}
+          education={education}
+          setPage={setPage}
+          handleChange={handleChange}
+        />
       )}
     </div>
   );
