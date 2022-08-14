5809bf194bddacdcfe8e74a911363bac7041cd5b
mahirlabibdihan <mahirlabibdihan@gmail.com>
2022-08-15 00:48:06 +0600

    Join request, course enroll request added

--- src/api/studentsApi.js
+++ src/api/studentsApi.js
@@ -9,8 +9,19 @@ class StudentsApi extends Api {
   getEnrolledStudentsList = async (data) => {
     return await this.post(`/students/enrolled_list`, { course: data });
   };
+  getPendingEnrolls = async (data) => {
+    console.log("=>", data);
+    return await this.post(`/students/pending_enrolls`, {
+      course: data,
+    });
+  };
   getMembersList = async (coaching_id) => {
     return await this.post(`/students/members_list`, {
+      coaching_id: coaching_id,
+    });
+  };
+  getJoinRequests = async (coaching_id) => {
+    return await this.post(`/students/join_requests`, {
       coaching_id: coaching_id,
     });
   };
