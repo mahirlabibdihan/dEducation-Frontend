3bf1a320956a779e36553fbb550db99bb452ee8b
mahirlabibdihan <mahirlabibdihan@gmail.com>
2022-08-30 01:43:00 +0600

    Made like pending req form

--- src/components/Forms/StudentSearchForm.jsx
+++ src/components/Forms/StudentSearchForm.jsx
@@ -7,81 +7,186 @@ import { useSearchParams } from "react-router-dom";
 import GlobalContext from "../../store/GlobalContext";
 import CourseSelectionForm from "./CourseSelectionForm";
 import { Zoom } from "@mui/material";
+import { RestrictedButton } from "../Buttons";
 import "./styles.scss";
+import { StudentTypeField, CourseSelectionFields } from "../InputFields";
 const coachingController = new CoachingController();
 const courseController = new CourseController();
 const StudentSearchForm = () => {
+  /*==========================*/
   const globalCtx = useContext(GlobalContext);
-  const [coachingsList, setCoachingsList] = useState([
-    { NAME: "Tution", COACHING_ID: -1 },
-  ]);
+  const [coachingsList, setCoachingsList] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
   const [classList, setClassList] = useState([]);
   const [subjectList, setSubjectList] = useState([]);
   const [batchList, setBatchList] = useState([]);
-  const [values, setValues] = useState({
-    coaching: -1,
-    class: "",
-    subject: "",
-    batch: "",
+  const initValues = () => ({
+    student_type:
+      searchParams.get("type") === null
+        ? "Accepted Tution"
+        : searchParams.get("type"),
+    coaching:
+      searchParams.get("coaching") === null ? "" : searchParams.get("coaching"),
+    class: searchParams.get("class") === null ? "" : searchParams.get("class"),
+    subject:
+      searchParams.get("subject") === null ? "" : searchParams.get("subject"),
+    batch: searchParams.get("batch") === null ? "" : searchParams.get("batch"),
   });
+  const [values, setValues] = useState(initValues());
   const setCoachingOptions = async () => {
     var res = await coachingController.getMyList();
-    const list = [{ NAME: "Tution", COACHING_ID: -1 }];
+    const list = [];
     for (let i = 0; i < res.data.length; i++) {
       list.push(res.data[i]);
     }
     setCoachingsList(list);
   };
   useEffect(() => {
-    if (coachingsList.length === 1) setCoachingOptions();
+    setValues(initValues());
   }, []);
 
+  useEffect(() => {
+    if (
+      values.student_type === "Coaching Member" ||
+      values.student_type === "Course Enrolled"
+    ) {
+      setCoachingOptions();
+    }
+  }, [values.student_type]);
   const handleChange = (prop) => (event) => {
     setValues({ ...values, [prop]: event.target.value });
   };
   const handleSearch = async (event) => {
     const tmp = new URLSearchParams();
-    if (values.coaching !== -1) {
-      tmp.set("coaching", values.coaching);
-    }
-    if (values.class !== "") {
-      tmp.set("class", values.class);
-    }
-    if (values.subject !== "") {
-      tmp.set("subject", values.subject);
-    }
-    if (values.batch !== "") {
-      tmp.set("batch", values.batch);
+    tmp.set("type", values.student_type);
+    if (
+      values.student_type === "Coaching Member" ||
+      values.student_type === "Course Enrolled"
+    ) {
+      if (values.coaching !== "") {
+        tmp.set("coaching", values.coaching);
+      }
+      if (values.student_type === "Course Enrolled") {
+        if (values.class !== "") {
+          tmp.set("class", values.class);
+        }
+        if (values.subject !== "") {
+          tmp.set("subject", values.subject);
+        }
+        if (values.batch !== "") {
+          tmp.set("batch", values.batch);
+        }
+      }
     }
     setSearchParams(tmp);
     globalCtx.setPendingUpdate(true);
   };
+  const setBatchOptions = async () => {
+    var res = await courseController.getBatchOptions(
+      values.coaching,
+      values.class,
+      values.subject
+    );
+    setBatchList(res.data);
+  };
+  const setClassOptions = async () => {
+    const res = await courseController.getClassOptions(values.coaching);
+    setClassList(res.data);
+  };
+  const setSubjectOptions = async () => {
+    const res = await courseController.getSubjectOptions(
+      values.coaching,
+      values.class
+    );
+    setSubjectList(res.data);
+  };
+  useEffect(() => {
+    setValues({
+      student_type: values.student_type,
+      coaching: "",
+      class: "",
+      subject: "",
+      batch: "",
+    });
+    if (values.student_type === "Accepted Tution") setCoachingsList([]);
+    setClassList([]);
+    setSubjectList([]);
+    setBatchList([]);
+  }, [values.student_type]);
+  useEffect(() => {
+    setValues({
+      student_type: values.student_type,
+      coaching: values.coaching,
+      class: "",
+      subject: "",
+      batch: "",
+    });
+    if (values.student_type === "Course Enrolled" && values.coaching !== "") {
+      setClassOptions();
+    } else {
+      setClassList([]);
+    }
+    setSubjectList([]);
+    setBatchList([]);
+  }, [values.coaching]);
+  useEffect(() => {
+    setValues({
+      student_type: values.student_type,
+      coaching: values.coaching,
+      class: values.class,
+      subject: "",
+      batch: "",
+    });
+    if (values.class !== "") {
+      setSubjectOptions();
+    } else {
+      setSubjectList([]);
+    }
+    setBatchList([]);
+  }, [values.class]);
+  useEffect(() => {
+    setValues({
+      student_type: values.student_type,
+      coaching: values.coaching,
+      class: values.class,
+      subject: values.subject,
+      batch: "",
+    });
+    if (values.subject !== "") {
+      setBatchOptions();
+    } else {
+      setBatchList([]);
+    }
+  }, [values.subject]);
+  /*==========================*/
+  //   console.log(values);
   return (
     <Zoom in={true}>
       <div className="student-search">
-        <h1 className="header">Filter</h1>
-        <Divider />
-        <CourseSelectionForm
-          values={values}
-          setValues={setValues}
-          coachingsList={coachingsList}
-          setCoachingOptions={setCoachingOptions}
-          classList={classList}
-          setClassList={setClassList}
-          subjectList={subjectList}
-          setSubjectList={setSubjectList}
-          batchList={batchList}
-          setBatchList={setBatchList}
+        <StudentTypeField
+          value={values.student_type}
           handleChange={handleChange}
         />
-        <Button
-          variant="contained"
-          className="blue-button full-width"
+        <CourseSelectionFields
+          values={values}
+          coachingsList={coachingsList}
+          classList={classList}
+          subjectList={subjectList}
+          batchList={batchList}
+          handleChange={handleChange}
+        />
+        <RestrictedButton
+          isDisabled={
+            (values.student_type === "Coaching Member" &&
+              values.coaching === "") ||
+            (values.student_type === "Course Enrolled" &&
+              (values.coaching === "" ||
+                values.class === "" ||
+                values.subject === ""))
+          }
           onClick={handleSearch}
-        >
-          Apply
-        </Button>
+          label="Apply"
+        />
       </div>
     </Zoom>
   );
